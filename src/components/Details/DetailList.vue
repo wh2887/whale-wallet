b90b2b3a6c698f3852556fac06bdf6a83cfa6553
McCall <1059511459@qq.com>
2020-08-05 12:21:38 +0800

    使用dayjs对日期的显示处理

--- src/components/Details/DetailList.vue
+++ src/components/Details/DetailList.vue
@@ -2,7 +2,7 @@
     <ol class="list">
         <li v-for="(group,index) in dataSource" :key="index">
             <div class="top">
-                <h3>{{group.title}}</h3>
+                <h3>{{beautify(group.title)}}</h3>
                 <h3>支： 30.00</h3>
             </div>
             <ol class="bottom" v-for="item in group.items" :key="item.id">
@@ -23,6 +23,7 @@
 
 <script lang="ts">
   import Vue from 'vue';
+  import dayjs from 'dayjs';
   import {Component, Prop} from 'vue-property-decorator';
   import IconWithBorder from '@/components/IconWithBorder.vue';
 
@@ -31,6 +32,24 @@
   })
   export default class DetailList extends Vue {
     @Prop() dataSource!: object;
+
+    beautify(string: string) {
+      const day = dayjs(string);
+      const now = dayjs();
+      if (day.isSame(now, 'day')) {
+        return '今天';
+      } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+        return '昨天';
+      } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+        return '前天';
+      }else if (day.isSame(now,'year')){
+        return day.format('MM月DD日')
+      }
+      else {
+        return day.format('YYYY年MM月DD日');
+      }
+
+    }
 
   }
 </script>
@@ -72,6 +91,7 @@
                         > :first-child {
                             text-align: start;
                         }
+
                         > :last-child {
                             color: #7D7878;
                         }
